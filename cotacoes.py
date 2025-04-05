33523131bf0b13e6a6aa369c5057d61b183bfe09
Ricardo Esdra <riesdra@zoho.com>
2025-04-05 19:16:45 -0300

    correção do requirements, tirei o cupshelper

--- cotacoes.py
+++ cotacoes.py
@@ -35,18 +35,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
-'''
-requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-requisicao_dic = requisicao.json()
-
-# Extraindo as cotações e convertendo para float
-cotacao_dolar = float(requisicao_dic['USDBRL']['bid'])
-cotacao_euro = float(requisicao_dic['EURBRL']['bid'])
-cotacao_btc = float(requisicao_dic['BTCBRL']['bid'])
-
-# Exibindo as cotações com 2 casas decimais
-print(f'Cotação Atualizada. {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}')
-print(f'Dólar: R${cotacao_dolar:.2f}')
-print(f'Euro: R${cotacao_euro:.2f}')
-print(f'BTC: R${cotacao_btc:,.2f}')
-'''
